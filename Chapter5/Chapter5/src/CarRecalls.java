e888ef0b60c1664f785d35a8cebd9cd72d261c96
John Heck <123605588+Jcheck100@users.noreply.github.com>
2023-11-03 08:35:39 -0400

    Update CarRecalls.java

--- Chapter5/Chapter5/src/CarRecalls.java
+++ Chapter5/Chapter5/src/CarRecalls.java
@@ -1,29 +1,33 @@
 import java.util.*;
+
 public class CarRecalls {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-   int modelNumber;
-   boolean defective = false;
+        int modelNumber;
+        boolean defective = false;
 
-   System.out.print("Please input your cars model number: ");
-modelNumber = input.nextInt();
+        System.out.print("Please input your cars model number: ");
+        modelNumber = input.nextInt();
 
- if (modelNumber == 119 || modelNumber ==179 || modelNumber ==221 || modelNumber ==780 || modelNumber >= 189 && modelNumber <= 195){
-    defective = true;
- }else{
-    defective = false;
- }
+        switch (modelNumber) {
+            case 119:
+                defective = true;
+            case 179:
+                defective = true;
+            case 221:
+                defective = true;
+            case 780:
+                defective = true;
+        }
 
-    if (defective == true){
-    System.out.println("Your car has been recalled.");
-    } else {
-        if(defective == false){
-    System.out.println("No recalls for this vehicle.");
+        if (modelNumber >= 189 && modelNumber <= 196) {
+            defective = true;
+        }
+
+        if (defective == true) {
+            System.out.println("Your car has been recalled.");
+        } else {
+            System.out.println("No recalls for this vehicle.");
         }
     }
 }
-}
-
-
-
-
