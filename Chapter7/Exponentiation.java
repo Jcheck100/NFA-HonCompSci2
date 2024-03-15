9d745f81359d53d52040c1a9ebb03d558f09ee1e
Jcheck100 <Jcheck100@gmail.com>
2024-03-15 08:33:51 -0400

    Exponentiation int instead of void

--- Chapter7/Exponentiation.java
+++ Chapter7/Exponentiation.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class Exponentiation {
     static Scanner input = new Scanner(System.in);
+
     public static void main(String[] args) {
         System.out.println("Enter base number: ");
         int baseNumber = input.nextInt();
@@ -9,15 +10,16 @@ public class Exponentiation {
         int exponent = input.nextInt();
         int finalNumber = baseNumber;
 
-        powerOf(baseNumber, exponent, finalNumber);
+        System.out.println(powerOf(baseNumber, exponent, finalNumber));
 
     }
-    public static void powerOf(int baseNumber, int exponent, int finalNumber){
-        for (int i = 0 ; i < exponent - 1 ; i++){
+
+    public static int powerOf(int baseNumber, int exponent, int finalNumber) {
+        for (int i = 0; i < exponent - 1; i++) {
             finalNumber *= baseNumber;
         }
-    
-        System.out.println(finalNumber);
+
+        return (finalNumber);
 
     }
 }
